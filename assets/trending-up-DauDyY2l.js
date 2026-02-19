95e261124eefa383f44e8f5a318e20f45c2d755a
techkreateup <techkreateup@users.noreply.github.com>
2026-02-19 09:31:12 +0000

    deploy: 485ddc8afa52139f050ba8ed18f886ab57319206

---
+++ assets/trending-up-DauDyY2l.js
@@ -0,0 +1,11 @@
+import{y as o}from"./index-CSh6C4Ty.js";/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M12 15V3",key:"m9g1x1"}],["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["path",{d:"m7 10 5 5 5-5",key:"brsn70"}]],d=o("download",n);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],e=o("trending-up",t);export{d as D,e as T};
