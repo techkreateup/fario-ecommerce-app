269b50d521cc59464111f4d43f46b02f8c3fbbcf
techkreateup <reachkreateup@gmail.com>
2026-03-26 14:18:17 +0530

    fix: removed overflow-hidden from header to restore profile dropdown visibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -102,7 +102,7 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className={`fixed top-0 left-0 right-0 z-[1000] border-b flex items-center overflow-hidden ${headerBg}`}>
+      <header className={`fixed top-0 left-0 right-0 z-[1000] border-b flex items-center ${headerBg}`}>
         <div className="container mx-auto px-4 md:px-6 lg:px-10 relative flex justify-between items-center w-full">
 
           <NavLink to="/" className="flex-1 flex items-center gap-3 lg:gap-4 relative z-[60] group">
